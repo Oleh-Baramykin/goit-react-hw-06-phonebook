ffd329563a7e86b0125b3eefaf042582ff68d968
Oleh Baramykin <olegix7891@ukr.net>
2023-02-23 11:16:27 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,16 +15,15 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (parsedContacts) this.setState({ contacts: parsedContacts });
+  }
+
   componentDidUpdate(_, prevState) {
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
-  }
-
-  componentDidMount() {
-    const contacs = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacs);
-    this.setState({ contacts: parsedContacts });
   }
 
   addContact = (values, { resetForm }) => {
